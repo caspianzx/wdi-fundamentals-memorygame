d7917611b3960a8e5b5f18efb5a9c0c2e927b591
caspianzx <caspianzx@gmail.com>
2019-06-14 18:02:18 +0800

    created game logic

---
+++ memory_game/js/main.js
@@ -0,0 +1,8 @@
+console.log("up and running!");
+
+const cardOne = "king";
+const cardTwo = "king";
+const cardThree = "queen";
+const cardFour = "queen";
+
+console.log( "User flipped " + cardOne);
